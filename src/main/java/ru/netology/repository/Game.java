0bb1a3c07f0cf64b698ff8528dbe4b6d5dc1c99d
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-05-24 19:57:08 +0400

    add HashMap

--- src/main/java/ru/netology/repository/Game.java
+++ src/main/java/ru/netology/repository/Game.java
@@ -3,37 +3,23 @@ package ru.netology.repository;
 import ru.netology.domain.NotRegisteredException;
 import ru.netology.domain.Player;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
-    private List<Player> players = new LinkedList<>();
+    private Map<String, Player> players = new HashMap<>();
 
-    public List<Player> findAll() {
-        return players;
-    }
-
-    public Player findByName(String name) {
-        for (Player player : players) {
-            if (player.getName()
-                    .equals(name)) {
-                return player;
-            }
-        }
-        return null;
-    }
-
-    public void registerAll(List<Player> players) {
-        this.players.addAll(players);
+    public void register(Player player) {
+        this.players.put(player.getName(), player);
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = findByName(playerName1);
-        Player player2 = findByName(playerName2);
-        if (findByName(playerName1) == null) {
+        Player player1 = players.get(playerName1);
+        Player player2 = players.get(playerName2);
+        if (player1 == null) {
             throw new NotRegisteredException(playerName1);
         }
-        if (findByName(playerName2) == null) {
+        if (player2 == null) {
             throw new NotRegisteredException(playerName2);
         }
         if (player1.getStrength() > player2.getStrength()) {
@@ -41,8 +27,7 @@ public class Game {
         }
         if (player1.getStrength() < player2.getStrength()) {
             return 2;
-        } else {
-            return 0;
         }
+        return 0;
     }
 }
